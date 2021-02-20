0b5fd5afc23e57a32a6a5d001108c386b9da6810
Dominick Nelson <60334994+nicknelson97@users.noreply.github.com>
2021-02-20 14:51:52 -0800

    Add files via upload

--- Menu.php
+++ Menu.php
@@ -7,18 +7,21 @@ class Menu {
     public $name = "";
     public $price = 0;
     public $desc = "";
-    public $qty=0;
+    public $image = "";
+    public $qty = 0;
+    //public $extra = 0;
     
-     public function __construct($name,$price,$desc)
+     public function __construct($name,$price,$desc,$image)
     {
         $this->name = $name;
         $this->price = $price;
         $this->desc = $desc;
+        $this->image = $image;
         $this->qty = 0;
-        
+        //$this->extra = 0;
     }
 }
 
-    $items[] = new Menu("item1",1.00,"This is the item description");
-    $items[] = new Menu("item2",5.00,"second item");
-    $items[] = new Menu("item3",4.00,"second item");
+    $items[] = new Menu("Salmon-Bowl",8.00,"Brown rice, soy sauce, avocado, chili garlic sauce, salmon fillets","includes/images/poke300x376.png");
+    $items[] = new Menu("Salmon-Taco",4.00,"Salmon, corn tortillas, lime, red onion, olive oil","includes/images/taco300x376.jpg");
+    $items[] = new Menu("Salmon-Poke",9.00,"Sockeye salmon, plain greek yogurt, apple cider vinegar, soy sauce, sesame oil","includes/images/bowl300x376.jpg");
