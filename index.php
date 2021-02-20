9b3b039a7289f5a6e76036030fb7fd03d2246c8d
Dominick Nelson <60334994+nicknelson97@users.noreply.github.com>
2021-02-20 14:49:00 -0800

    Added sub total and tax

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
+include('includes/header.php');
 include('Menu.php');
+
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-?>
 
-<!DOCTYPE html>
-<html lang="en-us">
-    
+
+
+
+?>
+  
+<div>
     <fieldset name="itemList">
-    <legend>This is my product list</legend>
+    <legend>Fresh Salmon Menu</legend>
         
         <form action="" method="post">
             
@@ -18,9 +22,10 @@ error_reporting(E_ALL);
         <?php 
             foreach($items as $item){
                 echo '
-                    <h3>' . $item->name . '</h3>
-                    <p>' . $item->desc . ' - ' . '$' . $item->price .'</p><br>
-                    <select name="'. $item->name . '" required title="0" tabindex="15">
+                    <img src="'.$item->image.'" alt="'.$item->name.'">
+                    <h3>'.$item->name.'</h3>
+                    <p>$'.$item->price.' - ' .''.$item->desc.'</p><br>
+                    <select name="'.$item->name.'" required title="0" tabindex="15">
                     <option value="0">Quantity</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -28,21 +33,27 @@ error_reporting(E_ALL);
                         <option value="4">4</option>
                         <option value="5">5</option>
                     </select>
-                ';
+                <br><br>';
+                
                 if(isset($_POST[$item->name])){
                     $item->qty = intval($_POST[$item->name]);
                 }
+                //if(isset($_POST['extras'])){
+                //    $item->extra = intval($_POST['extras']);
+                //}
             }
         ?>
-        <br><br><input type="submit" value="Total">
+        <br><br><input type="submit" value=" Checkout Total ">
             
         </form>
         
     </fieldset>
-    
+    </div>
+
+    <div>
     <fieldset name="selectedItems">
-    <legend>Selected Items</legend>
-        <h2>You Chose: <br><br>
+    <legend>Checkout</legend>
+        <h2>Items ordered: <br><br></h2>
         <?php 
             foreach($items as $item){
                 //if(isset($_POST[$item->qty])){
@@ -55,18 +66,21 @@ error_reporting(E_ALL);
                     //}
                 }
             }
-        ?>
-        <h2><br>Your Total: 
-            <?php 
-                $total = 0;
-
-                foreach($items as $item){
+        $total = 0;
+        foreach($items as $item){
                     $sub = $item->price * $item->qty;
                     $total = $total + $sub;
                 }
-            ?>
-        <h2>
-        <?php echo '<p>$' .round(($total + 1.065), 2) . '</p>'; ?>
-    
-    
-</html>
+        ?>
+        <h2><br>Sub total:<br>
+           <?php echo '<p>$'.round(($total), 2).'</p>'; ?>
+        </h2>
+        <h2><br>Tax:<br>
+            <?php echo '<p>$'.round(($total * .065), 2).'</p>'; ?>
+        </h2>
+        <h2><br>Your Total: 
+            <?php echo '<p>$'.round(($total * 1.065), 2).'</p>'; ?>
+        </h2>
+            </fieldset>
+    </div>
+<?php include('includes/footer.php'); ?>
