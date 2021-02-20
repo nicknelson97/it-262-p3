be44eff2539c365a65e10303cd1bcf8ff6c49989
Dominick Nelson <60334994+nicknelson97@users.noreply.github.com>
2021-02-20 14:50:31 -0800

    redid file structure

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Seattle Central Food Truck</title>
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+    <header>
+            <img id="logo" src="includes/images/logo.png" alt="logo">
+            <h1>Seattle Central Food Truck</h1>
+    </header>
+<body>
